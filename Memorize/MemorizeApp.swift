eea8242c35100ca7fd3b352b68ce59682ce85395
alves-patrick <patrickalvesarts@gmail.com>
2023-11-02 09:02:21 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Patrick Alves on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
